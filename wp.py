70ffee03314ee040ce54faf05517c9fa08d85883
Imtiaz Hossain <imtiazhossain9734@gmail.com>
2025-10-18 16:23:57 +0530

    Delete wp.py
    unwanted file

--- wp.py
+++
@@ -1,7 +0,0 @@
-import pywhatkit as pwk
-phone_number= "+919883319587"
-message="It's an automated text"
-#pwk.sendwhatmsg(phone_number,message)
-pwk.sendwhatmsg(phone_number,message,time_hour=1,time_min=49)
-
-
